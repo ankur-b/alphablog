a838951ab37df376ae9f5bd76659ab825d23406a
ankur-b <ankurbarve29@gmail.com>
2023-06-15 23:32:33 +0530

    Refactor code using partials and make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController  
+  before_action :set_article ,only: [:show,:edit,:destroy,:update]
+
   def show
-    @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article
@@ -18,12 +19,11 @@ class ArticlesController < ApplicationController
     end
   end
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
@@ -32,8 +32,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    print(@article.destroy)
     redirect_to @article
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
